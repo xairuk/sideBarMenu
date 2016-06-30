7cd1d430be9a82bd0b641584afad0574dce609d4
XairukVM <xairukvm@xairukvms-mac.local>
2016-06-30 18:03:20 -0500

    Initial Commit

---
+++ slideMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  slideMenu
+//
+//  Created by XairukVM on 30/06/16.
+//  Copyright © 2016 XairukVM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
